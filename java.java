2e0bcdbb3f246388a93065964784df8574660001
rahulgreddy <51025912+rahulgreddy@users.noreply.github.com>
2019-06-11 10:22:19 -0400

    Add files via upload

---
+++ java.java
@@ -0,0 +1,9 @@
+public class java
+{
+public static void main(String[] args) 
+{ 
+int a=5, b=10, c=10, d=10, e=10, sum;
+sum=a + b + c + d + e;
+System.out.println(" sum of two numbers: "+sum);
+}
+}
